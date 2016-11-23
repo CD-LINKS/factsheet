dee9952d995dea6c7c0ea30700bbf495d5697246
Hel1vs <heleen.vansoest@pbl.nl>
2016-11-23 12:32:07 +0100

    Added a function and section for new graph showing energy indicators, comparison between scenarios

--- src/functions/plot_functions.R
+++ src/functions/plot_functions.R
@@ -300,3 +300,26 @@ plot_bar_facet <- function(reg, dt, vars, cats, out=cfg$outdir, lab="Title", tit
   ggsave(file=paste0(out,"/",file_pre,"_",reg,cfg$format),p, width=7, height=8, dpi=120)
   return(p)
 }
+
+#############################################################
+###################### plot_bar_facet2 #######################
+#############################################################
+
+plot_bar_facet2 <- function(reg, dt, vars, cats, out=cfg$outdir, lab="Title", title="Title",file_pre="def",ylim=NA,xlim=NA){
+  #select data
+  dt <- dt[region==reg & Category %in% cats & variable %in% vars]
+  unitsy <- paste0("(",unique(dt[variable%in%vars]$unit),")    ")
+  unitsy <- paste(rev(unitsy),sep='',collapse='')
+  
+  p = ggplot(dt,aes(x=Category, y=value, fill=model))
+  p = p + facet_grid(variable ~ region,scales="free_y")
+  p = p + geom_bar(stat="identity",position=position_dodge(width=0.66),width=0.66)
+  #p = p + coord_flip()
+  p = p + xlab("")
+  if (!all(is.na(ylim))){p = p + ylim(ylim)} #manual y-axis limits
+  p = p + scale_fill_manual(values=plotstyle(as.character(unique(dt$model))))
+  p = p + ylab(paste(unitsy))
+  p = p + ggplot2::theme_bw()+ theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  ggsave(file=paste0(out,"/",file_pre,"_",reg,cfg$format),p, width=7, height=8, dpi=120)
+  return(p)
+}
