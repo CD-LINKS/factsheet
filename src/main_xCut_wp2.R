f66d88ccfb3966c34944fc1799ecae670bf070f6
Hel1vs <heleen.vansoest@pbl.nl>
2016-11-29 16:34:53 +0100

    Also add reference budgets to prevent error

--- src/main_xCut_wp2.R
+++ src/main_xCut_wp2.R
@@ -38,6 +38,13 @@ if(!file.exists(cfg$outdir)) {
 ####################### Load stock-taking data ##############
 #############################################################
 
+#input reference budgets for national scenarios:
+ref_budgets <- data.frame(region =c(rep("IND",4),rep("BRA",4), rep("JPN",4),rep("RUS",4) ,rep("CHN",4), rep("EUR",4),rep("USA",4)),scenario=rep(c("high","high","low","low"),7),border=rep(c("min","max"),14),
+                          value=c(125*1.05,125*.95,90*1.05,90*.95, 42*1.05,42*.95,22*1.05,22*.95,
+                                  36.5*1.05,36.5*.95,31.47*1.05,31.47*.95, 50.5*1.05,50.5*.95,45*1.05,45*.95,
+                                  400*1.05,400*.95,290*1.05,290*.95, 112*1.05,112*.95,95*1.05,95*.95,
+                                  100*1.05,100*.95,100*1.05,100*.95))
+
 #if processed data is already available, just load it. To redo processing (e.g. after adding new calculated variable, set b.procdata = TRUE)
 if (file.exists(paste0("data/",cfg$infile,"_proc.Rdata")) & !b.procdata) {
   cat("Loading processed data from file", paste0("data/",cfg$infile,".Rdata"),"\n",
