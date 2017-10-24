76e71a634e807c02b9184218068f02cf7ed6bbc9
Hel1vs <heleen.vansoest@pbl.nl>
2017-10-24 17:40:08 +0200

    Now using a separate adjust_reporting file here, so other scripts can use the old file without paper-specific adjustments

--- src/paper_graphs.R
+++ src/paper_graphs.R
@@ -98,7 +98,7 @@ if (file.exists(paste0("data/",cfg$infile,"_proc.Rdata")) & !b.procdata) {
   all <- factor.data.frame(all)
   
   # model specific adjustments
-  source("adjust_reporting_indc.R")
+  source("adjust_reporting_indc_Mark.R")
   
   #### add variables
   all <- add_variables(all,scens)
