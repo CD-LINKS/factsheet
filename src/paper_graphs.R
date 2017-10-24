be3d7fe2460825036cc5964c0619083740cc57a8
Hel1vs <heleen.vansoest@pbl.nl>
2017-10-24 18:21:02 +0200

    New snapshot (and excluding Russia from national fact sheet generation for the moment - as it crashes without RU-TIMES submission)

--- src/paper_graphs.R
+++ src/paper_graphs.R
@@ -15,7 +15,7 @@ library(gridExtra) #arrangeGrob
 
 #source configuration file for region-specific data
 source("settings/config_xCut.R")
-cfg$infile <- "cdlinks_compare_20171017-161933"
+cfg$infile <- "cdlinks_compare_20171024-085757"
 cfg$outdir    <- "paper graphs"#source function for factorizing data frames
 source("functions/factor.data.frame.R")
 # source functions process_data() and add_variables()
