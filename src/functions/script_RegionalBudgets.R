72f135151fbec2144f2fe309c1333f091f59911a
Hel1vs <heleen.vansoest@pbl.nl>
2017-10-28 22:23:20 +0200

    Different selection of scenarios for budget graph for synthesis paper

--- src/functions/script_RegionalBudgets.R
+++ src/functions/script_RegionalBudgets.R
@@ -6,8 +6,8 @@ vars <- "Emissions|CO2"
 cats <- c("NoPOL","NPi","INDC", "2030_high", "2030_low", "2020_high", "2020_low", "2020_verylow")
 # scens <- c( "NPi", "INDCi", "2020_1800","2030_1800", "2020_1000", "2030_1000", "2020_400",
 #             "2030_1000")
-scens2deg <- c("INDC", "2030_high", "2030_low", "2020_verylow")
-
+#scens2deg <- c("INDC", "2030_high", "2030_low", "2020_verylow")
+scens2deg <- c("NPi", "2030_low", "2020_low", "2020_verylow")
 
 # v_co2reg <- v_procDataScen %>% filter (variable == "Emissions|CO2", region %in% regs & !is.na(value))
 # v_co2reg_cum <- calcCumulatedDiscount(as.quitte(v_co2reg),discount = 0,fixYear = 2010,nameVar="Emissions|CO2") %>% filter (period==2050)
@@ -69,17 +69,18 @@ v_plot$Category =  factor(v_plot$Category, levels = scens2deg, ordered = T)
 ggplot() +
     geom_boxplot(data=v_plot[v_plot$Scope=="global",],aes(x=Category,y=`CO2rel2010`, fill = Category), outlier.size = 0, coef = 3) +
   geom_point(data=v_plot[v_plot$Scope=="national",],aes(x=Category,y=`CO2rel2010`, shape = model),color = 'white',size = 2) +
-    geom_point(data=v_plot[v_plot$Scope=="national",],aes(x=Category,y=`CO2rel2010`, shape = model),color = 'red',size = 3, stroke = 1) +
+    geom_point(data=v_plot[v_plot$Scope=="national",],aes(x=Category,y=`CO2rel2010`, shape = model),color = 'black',size = 3, stroke = 1) +
   #    geom_point(data=v_plot,aes(x=scenario,y=`CO2rel2010` ,shape=model)) +
     ggtitle(paste0("CO2 total (2011-2050 rel. to 2010)")) + ylab("Emission Years") +
     xlab ("") +
     scale_shape_manual(values = 1:nlevels(v_plot$model)) +
     scale_fill_manual(values= plotstyle(scens2deg),
-                      labels=c("INDC", "med2°C","below2°C", "1.5°C"),
+                      #labels=c("INDC", "med2°C","below2°C", "1.5°C"),
+                      labels=c("NPi", "NDC1000","NPi1000", "NPi400"),
                       name="") +
     theme( strip.background = element_blank(), plot.background = element_rect(fill     = "transparent",colour = NA ),
            panel.background = element_rect(fill     = "transparent",colour = NA )) +
-    scale_x_discrete(labels=c("INDC", "med2°C","below2°C", "1.5°C")) +
+    scale_x_discrete(labels=c("NPi", "NDC1000","NPi1000", "NPi400")) + #labels=c("INDC", "med2°C","below2°C", "1.5°C")
 #    theme(axis.text.y  = element_blank()) +
   facet_grid(  . ~ region) +
   coord_cartesian(ylim = c(0,80)) +
@@ -133,17 +134,18 @@ v_plot$Category =  factor(v_plot$Category, levels = scens2deg, ordered = T)
 ggplot() +
   geom_boxplot(data=v_plot[v_plot$Scope=="global",],aes(x=Category,y=CO2Budget, fill = Category), outlier.size = 0, coef = 3) +
   geom_point(data=v_plot[v_plot$Scope=="national",],aes(x=Category,y=CO2Budget, shape = model),color = 'white',size = 2) +
-  geom_point(data=v_plot[v_plot$Scope=="national",],aes(x=Category,y=CO2Budget, shape = model),color = 'red',size = 3, stroke = 1) +
+  geom_point(data=v_plot[v_plot$Scope=="national",],aes(x=Category,y=CO2Budget, shape = model),color = 'black',size = 3, stroke = 1) +
   #    geom_point(data=v_plot,aes(x=scenario,y=`CO2rel2010` ,shape=model)) +
   ggtitle(paste0("CO2 total (2011-2050)")) + ylab("CO2 Budget [GtCO2]") +
   xlab ("") +
   scale_shape_manual(values = 1:nlevels(v_plot$model)) +
   scale_fill_manual(values= plotstyle(scens2deg),
-                    labels=c("INDC", "med2°C","below2°C", "1.5°C"),
+                    #labels=c("INDC", "med2°C","below2°C", "1.5°C"),
+                    labels=c("NPi", "NDC1000","NPi1000", "NPi400"),
                     name="") +
   theme( strip.background = element_blank(), plot.background = element_rect(fill     = "transparent",colour = NA ),
          panel.background = element_rect(fill     = "transparent",colour = NA )) +
-  scale_x_discrete(labels=c("INDC", "med2°C","below2°C", "1.5°C")) +
+  scale_x_discrete(labels=c("NPi", "NDC1000","NPi1000", "NPi400")) + #labels=c("INDC", "med2°C","below2°C", "1.5°C")
   #    theme(axis.text.y  = element_blank()) +
   facet_grid(  . ~ region) +
   theme( strip.background = element_blank(), plot.background = element_rect(fill = "transparent",colour = NA ),
@@ -158,17 +160,18 @@ ggsave(file=paste0("plots/","CO2_EmissionBudgets","_multiregbox.png"),
 ggplot() +
   #geom_boxplot(data=v_plot[v_plot$Scope=="global",],aes(x=Category,y=CO2Budget, fill = Category), outlier.size = 0, coef = 3) +
   geom_point(data=v_plot[v_plot$Scope=="national",],aes(x=Category,y=CO2Budget, shape = model),color = 'white',size = 2) +
-  geom_point(data=v_plot[v_plot$Scope=="national",],aes(x=Category,y=CO2Budget, shape = model),color = 'red',size = 3, stroke = 1) +
+  geom_point(data=v_plot[v_plot$Scope=="national",],aes(x=Category,y=CO2Budget, shape = model),color = 'black',size = 3, stroke = 1) +
   #    geom_point(data=v_plot,aes(x=scenario,y=`CO2rel2010` ,shape=model)) +
   ggtitle(paste0("CO2 total (2011-2050)")) + ylab("CO2 Budget [GtCO2]") +
   xlab ("") +
   scale_shape_manual(values = 1:nlevels(v_plot$model)) +
   scale_fill_manual(values= plotstyle(scens2deg),
-                    labels=c("INDC", "med2°C","below2°C", "1.5°C"),
+                    #labels=c("INDC", "med2°C","below2°C", "1.5°C"),
+                    labels=c("NPi", "NDC1000","NPi1000", "NPi400"),
                     name="") +
   theme( strip.background = element_blank(), plot.background = element_rect(fill     = "transparent",colour = NA ),
          panel.background = element_rect(fill     = "transparent",colour = NA )) +
-  scale_x_discrete(labels=c("INDC", "med2°C","below2°C", "1.5°C")) +
+  scale_x_discrete(labels=c("NPi", "NDC1000","NPi1000", "NPi400")) + #labels=c("INDC", "med2°C","below2°C", "1.5°C")
   #    theme(axis.text.y  = element_blank()) +
   facet_grid(  . ~ region) +
   theme( strip.background = element_blank(), plot.background = element_rect(fill = "transparent",colour = NA ),
