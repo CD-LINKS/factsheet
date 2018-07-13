4956a0ecdcd1fde64c5531d12e86d29771e90cc8
Hel1vs <heleen.vansoest@pbl.nl>
2018-07-13 15:54:56 +0200

    add CAGR for 2020-2050 (for WP2.3 gap indicator work)

--- src/functions/calcRate.R
+++ src/functions/calcRate.R
@@ -18,7 +18,7 @@ calcRate = function(data,var){
   rate$unit = "%/Year"
   rate$period="2030-2050"
   
-  ### For 2030-2050
+  ### For 2050-2100
   rate2=spread(dat[period %in% c('2050','2100')],period,value)
   rate2=rate2 %>% mutate(rate=(((`2100`/`2050`)^(1/50))-1)*100)
   rate2=gather(rate2,period,value, c(`2050`,`2100`,rate))
@@ -29,11 +29,24 @@ calcRate = function(data,var){
   rate2$variable = paste("Rate of Change|",rate2$variable)
   rate2$unit = "%/Year"
   rate2$period="2050-2100"
+
+  ### For 2020-2050
+  rate3=spread(dat[period %in% c('2020','2050')],period,value)
+  rate3=rate3 %>% mutate(rate=(((`2050`/`2020`)^(1/30))-1)*100)
+  rate3=gather(rate3,period,value, c(`2020`,`2050`,rate))
+  rate3=data.table(rate3)
+  rate3=rate3[period=="rate"]
+  
+  #correctly specify remaining dimensions
+  rate3$variable = paste("Rate of Change|",rate3$variable)
+  rate3$unit = "%/Year"
+  rate3$period="2020-2050"
   
   #merge data
   setcolorder(rate,c('scenario','Category','Baseline','model','region','period','Scope','value','unit','variable'))
   setcolorder(rate2,c('scenario','Category','Baseline','model','region','period','Scope','value','unit','variable'))
-  data <- rbind(data, rate, rate2, fill=TRUE)
+  setcolorder(rate3,c('scenario','Category','Baseline','model','region','period','Scope','value','unit','variable'))
+  data <- rbind(data, rate, rate2, rate3, fill=TRUE)
   
   # convert to data.table / data.frame and return
   return(as.data.table(as.data.frame(data)))
