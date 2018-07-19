cee18b66fee88dff13c85f45588fd5eb5a208873
Hel1vs <heleen.vansoest@pbl.nl>
2018-07-19 16:12:09 +0200

    Moving / changing names to clean up folder structure for general IAMC version

---
+++ src/main_crosscut.R
@@ -0,0 +1,172 @@
+#############################################################
+########## Main file that loads and processes data ##########
+########## for x-Cut analyisis    ##########
+#############################################################
+
+library(reshape2)   # melt
+library(data.table) # setnames, nice view option
+library(dplyr)      # %>%
+library(tidyr)      # spread
+library(ggplot2)    # ggplot
+library(rmarkdown)  # render pdf
+library(directlabels) # year labels for scatter plots
+library(stringr) #str_replace_all
+
+#set working directory for R right if it is not by default (it is the right one by default if you open Rstudio by clicking on this main.R file)
+#setwd("D:/location-of-srcfolder-on-your-system")
+
+#source configuration file for region-specific data
+source("settings/config_xCut.R")
+cfg$infile <- "cdlinks_compare_20171127-154822"
+
+#source function for factorizing data frames
+source("functions/factor.data.frame.R")
+# source functions process_data() and add_variables()
+source("functions/data_processing.R")
+#source function overwrite for overwriting a dataframe for a subset of variables
+source("functions/overwrite.R")
+#source file with plot functions
+source("functions/plot_functions_wp2.R")
+
+# flag to process data, reprocess even if _proc.rdata file is available
+# set to true if you always want data re-processed
+b.procdata = T
+# flag to adjust reporting, set to T if you want to call the script with manual corrections of (missing) data, set to F if you don't want to use this functionality
+b.adjustrep = T
+
+# Create plot directory
+if(!file.exists(cfg$outdir)) {
+  dir.create(cfg$outdir, recursive = TRUE)
+}
+
+#############################################################
+####################### Load stock-taking data ##############
+#############################################################
+
+#if processed data is already available, just load it. To redo processing (e.g. after adding new calculated variable, set b.procdata = TRUE)
+if (file.exists(paste0("data/",cfg$infile,"_proc.Rdata")) & !b.procdata) {
+  cat("Loading processed data from file", paste0("data/",cfg$infile,"_proc.Rdata"),"\n",
+      "set b.procdata flag and re-run if you want to do the data processing again", "\n")
+  load(paste0("data/",cfg$infile,"_proc.Rdata"))
+  Sys.sleep(2)#give everybody the chance to read the above message
+} else {
+
+  if (file.exists(paste0("data/",cfg$infile,".Rdata"))) {
+    cat("Loading file", paste0("data/",cfg$infile,".Rdata"),"\n")
+    load(paste0("data/",cfg$infile,".Rdata"))
+  } else {
+    cat("Reading data from file",paste0("data/",cfg$infile,".csv"),"\n")
+    all <- invisible(fread(paste0("data/",cfg$infile,".csv"),header=TRUE))
+    save("all",file = paste0("data/",cfg$infile,".Rdata"))
+  }
+
+  
+  #reduce size of the data frame
+  vars <- fread("settings/variables.csv",header=TRUE,stringsAsFactors=FALSE,sep='\n')
+  all  <- all[VARIABLE %in% vars$variable & REGION %in% cfg$regions]
+
+
+  #####################################
+  ####### Process stock-taking data ###
+  #####################################
+  cat("Processing stocktaking data\n")
+
+  # Add information for new column "Category"
+  scens <- fread("settings/scen_categ_cdlinks.csv", header=TRUE)
+  #get rid of duplicated scenarios
+  scens <- scens[!duplicated(scens$scenario)]
+
+  #all <- all[!(MODEL=="GEM-E3_V1"&SCENARIO=="INDC")]
+  
+  # Change scenario names for some models to V4 to not mix up old global model results with new ones, while using latest version of other models (V3)
+  #Special case for RU_TIMES: no V3/V4 at all
+  all[MODEL %in% c("RU-TIMES 3.2")]$SCENARIO <- 
+    paste(all[MODEL %in% c("RU-TIMES 3.2")]$SCENARIO,'_V4',sep="")
+  #Special case for GEM-E3: select the recGenTaxation scenarios for standard analysis
+  all[MODEL %in% c("GEM-E3")&SCENARIO%in%c("INDCi_recGenTaxation_V4")]$SCENARIO <- str_replace_all(
+    all[MODEL %in% c("GEM-E3")& SCENARIO%in%c("INDCi_recGenTaxation_V4")]$SCENARIO,"INDCi_recGenTaxation_V4","INDCi_V4")
+  all[MODEL %in% c("GEM-E3")&SCENARIO%in%c("INDC2030i_1000_recGenTaxation_V4")]$SCENARIO <- str_replace_all(
+    all[MODEL %in% c("GEM-E3")& SCENARIO%in%c("INDC2030i_1000_recGenTaxation_V4")]$SCENARIO,"INDC2030i_1000_recGenTaxation_V4","INDC2030i_1000_V4")
+  all[MODEL %in% c("GEM-E3")&SCENARIO%in%c("NPi2020_1000_recGenTaxation_V4")]$SCENARIO <- str_replace_all(
+    all[MODEL %in% c("GEM-E3")& SCENARIO%in%c("NPi2020_1000_recGenTaxation_V4")]$SCENARIO,"NPi2020_1000_recGenTaxation_V4","NPi2020_1000_V4")
+  all[MODEL %in% c("GEM-E3")&SCENARIO%in%c("NPi2020_400_recGenTaxation_V4")]$SCENARIO <- str_replace_all(
+    all[MODEL %in% c("GEM-E3")& SCENARIO%in%c("NPi2020_400_recGenTaxation_V4")]$SCENARIO,"NPi2020_400_recGenTaxation_V4","NPi2020_400_V4")
+  #Rest: use latest version V3 for national (and some global) models, rename to V4
+  all[MODEL %in% c("AIM/Enduse 3.0","AIM/Enduse[Japan]","China TIMES","COPPE-COFFEE 1.0","DNE21+ V.14","DNE21+ V.14 (national)","GCAM-USA_CDLINKS","India MARKAL","IPAC-AIM/technology V1.0","PRIMES_V1")]$SCENARIO <- str_replace_all(
+    all[MODEL %in% c("AIM/Enduse 3.0","AIM/Enduse[Japan]","China TIMES","COPPE-COFFEE 1.0","DNE21+ V.14","DNE21+ V.14 (national)","GCAM-USA_CDLINKS","India MARKAL","IPAC-AIM/technology V1.0","PRIMES_V1")]$SCENARIO,"_V3","_V4")
+   
+  #### from raw wide format to long format with additional columns
+  all <- process_data(all,scens)
+  
+  #re-factorize all character and numeric columns
+  all <- factor.data.frame(all)
+  
+  # #   print out summary of models-scenarios and variables
+  #   source("functions/SubmOverview.R")
+  #   
+  #   #produce pdf with analysis of data as-submitted
+  #   for (reg in c("JPN","BRA","CHN","IND","EU","RUS")){
+  #       cat("Producing graphs in graphs folder and INDC_national_subm_xxx.pdf in main folder\n")
+  #   render("national_scenarios.rmd",output_file=paste0("INDC_national_subm_",reg,".pdf"))
+  #   }
+  
+  
+  # model specific adjustments
+  if(b.adjustrep){source("adjust_reporting.R")}
+
+  #### add variables
+  all <- add_variables(all,scens)
+  
+  #correct scope for added variables
+  all[all$model %in% cfg$models_nat,]$Scope <- "national"
+  #special case GEM-E3: national model for EU, global for other regions
+  all[model=="GEM-E3"&region!="EU"]$Scope<-"global"
+  #special case DNE21+ V.14: only national protocol scenarios for JPN are "national", so the rest is global
+  # all[all$model == "DNE21+ V.14" & all$scenario %in% c("NoPolicy_V2","INDCi_V2","INDC2030i_1000_V2","INDC2030i_1600_V2",
+  #                                                      "INDC2030i_400_V2","NPi2020_1000_V2","NPi2020_1600_V2","NPi2020_400_V2")]$Scope <- "global"
+  # all[all$model == "DNE21+ V.14" & all$scenario %in% c("NPi_V2") & all$region!="JPN"] <- "global"
+  
+  # categorize national models
+  all[all$Scope=="national",]$model <- paste0("*",all[all$Scope=="national",]$model)
+  nat_models <- paste0("*",cfg$models_nat)
+  
+  #get rid of Historical duplicates
+  all <- all[Category!="Historical"]
+  
+  save("all",file = paste0("data/",cfg$infile,"_proc.Rdata"))
+
+}# end if-else: load and process stocktaking data
+
+#############################################################
+######### create fact sheet for national scenarios ##########
+#############################################################
+
+source("functions/plot_functions.R")
+
+for (reg in c("JPN","BRA","CHN","IND","EU","RUS")){
+  cat("Producing graphs in graphs folder and factsheet_national_adj_xxx.pdf in main folder\n")
+  render("markdown/national_scenarios.rmd",output_file=paste0("factsheet_national_adj_",reg,".pdf"))
+}
+
+#############################################################
+################## Do plots for cross-cut analysis ##########
+#############################################################
+source("functions/plot_functions_wp2.R")
+source("cross_cut.R")
+source("functions/RegionalBudgetsChecks2100.R")
+
+#############################################################
+################## create fact sheet for World ##############
+#############################################################
+
+source("settings/config_World.R")
+# Create plot directory
+if(!file.exists(cfg$outdir)) {
+  dir.create(cfg$outdir, recursive = TRUE)
+}
+
+render("markdown/factsheet_world.rmd",output_file=paste0("Factsheet_world.pdf"))
+
+# for (reg in c("JPN","BRA","CHN","IND","EUR","RUS")){
+# #source configuration file for region-specific data
+# source(paste0("settings/config_",reg,".R"))
