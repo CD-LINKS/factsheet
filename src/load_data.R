919dd215c58c018e14d6aa45292603c132667b26
Hel1vs <heleen.vansoest@pbl.nl>
2019-04-01 17:00:08 +0200

    using a snapshot containing MESSAGE for neutrality paper

--- src/load_data.R
+++ src/load_data.R
@@ -41,8 +41,8 @@ source(paste("settings/",config,".R",sep=""))
 
 #overwrite file to be used for analysis
 #cfg$infile    <- "cdlinks_compare_20190225-093329"
-#cfg$infile    <- "cdlinks_compare_20190123-155652"
-cfg$infile    <-  "cdlinks_compare_20190222-175217"
+cfg$infile    <- "cdlinks_compare_20190123-155652"
+#cfg$infile    <-  "cdlinks_compare_20190222-175217"
 
 #source function for factorizing data frames
 source("functions/factor.data.frame.R")
