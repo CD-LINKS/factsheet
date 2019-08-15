20396569978ddb99f9356198addec9995f9d32e6
Hel1vs <heleen.vansoest@pbl.nl>
2019-08-15 15:43:53 +0200

    instead of main_xCut, just use load_data and source the cross_cut_synthesis script for Roberto's synthesis paper revision

---
+++ src/source_synthesis_paper.R
@@ -0,0 +1,9 @@
+setwd("~/disks/local/factsheet/src")
+config <-"config_xCut"
+scencateg <- "scen_categ_V4"
+variables <- "variables_xCut"
+adjust <- "adjust_reporting_indc" 
+addvars <- T
+datafile <-"cdlinks_compare_20190813-112922"
+source("load_data.R")
+source("cross_cut_synthesis.R")
