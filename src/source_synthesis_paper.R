40ee31b697374833a47be35a5574b0f78cf6aea6
Hel1vs <heleen.vansoest@pbl.nl>
2019-10-18 15:39:53 +0200

    new snapshot

--- src/source_synthesis_paper.R
+++ src/source_synthesis_paper.R
@@ -4,6 +4,6 @@ scencateg <- "scen_categ_V4"
 variables <- "variables_xCut"
 adjust <- "adjust_reporting_indc" 
 addvars <- T
-datafile <-"cdlinks_compare_20190813-112922"
+datafile <-"cdlinks_compare_20191017-143619"
 source("load_data.R")
 source("cross_cut_synthesis.R")
