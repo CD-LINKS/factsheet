973981f6f14ffd78c3085b6df97aae40eb6ee4a6
Hel1vs <heleen.vansoest@pbl.nl>
2019-12-02 16:12:47 +0100

    new snapshot for Brazil (demand emissions for waterfall plot)

--- src/source_synthesis_paper.R
+++ src/source_synthesis_paper.R
@@ -4,6 +4,6 @@ scencateg <- "scen_categ_V4"
 variables <- "variables_xCut"
 adjust <- "adjust_reporting_indc" 
 addvars <- T
-datafile <-"cdlinks_compare_20191119-134837"
+datafile <-"cdlinks_compare_20191202-092954"
 source("load_data.R")
 source("cross_cut_synthesis.R")
